604c84860f47b27e4a2f56f326644c588dad9777
smanzambi <sean.manzambi@insa-rouen.fr>
2018-05-23 01:34:35 +0200

    Mise à jour

--- resources/views/layouts/connexion.php
+++ resources/views/layouts/connexion.php
@@ -1,9 +1,10 @@
 <!DOCTYPE HTML>
 <html>
   <head>
-    <title>Parrainage</title>
+    <title>Connexion</title>
     <meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="css/style.css"/>
     <link rel="stylesheet" href="/home/manzambi/Bureau/ASI 3.2/UML/html/fontawesome-free-5.0.11/web-fonts-with-css/css"/>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
     <style>
@@ -185,27 +186,27 @@
     </style>
   </head>
   <body>
-    <!-- Menu - Vertical -->
-    <!--  <div id = "menu-vertical">
-        <ul>
-          <li><a href="#"> Parrain/Fillot </a> </li>
-          <li><a href="#"> Profil </a> </li>
-          <li><a href="#"> Evénement </a> </li>
-          <li><a href="#">Agenda</a> </li>
-          <li><a href="#">Communication</a> </li>
-          <li><a href="#"><span id="home" class="fas fa-home"></span></a> </li>
-        </ul>
-      </div>
-    -->
-    <!-- Menu - Bannière -->
+    <!-- Bannière -->
     <section id="banniere">
       <div class="inner">
         <h1>Parrainage</h1>
         <p>Site de rencontre fillot/parrain n°1</p>
-        <ul class="actions">
-          <li><a href="inscription.html" class="bouton">Inscription</a></li>
-          <li><a href="connexion.php" class="bouton">Connexion</a></li>
-        </ul>
+        <form action="GET">
+          <fieldset>
+            <legend>Connexion</legend>
+            <table>
+              <tr>
+                <td><label> Nom d'utilisateur :</label></td>
+                <td><input type="text" class="box"name = "nom" id="nom" placeholder="Nom utilisateur"></td>
+              </tr>
+              <tr>
+                <td><label>Mot de passe :</label></td>
+                <td><input type="password" class="box"name = "password" id="password" placeholder="Mot de passe"></td>
+              </tr>
+            </table>
+          </fieldset>
+          <a href="home.html" class="bouton">Connexion</a>
+        </form>
       </div>
     </section>
   </body>

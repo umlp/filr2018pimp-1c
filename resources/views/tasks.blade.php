720f499ec9d06dd1676dec2c050129556048bb6b
smanzambi <sean.manzambi@insa-rouen.fr>
2018-05-22 19:00:37 +0200

    Mise à jour

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,78 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="col-sm-offset-2 col-sm-8">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    Nouvelle Tâche
-                </div>
-
-                <div class="panel-body">
-                    <!-- Display Validation Errors -->
-                    @include('common.errors')
-
-                    <!-- New Task Form -->
-                    <form action="{{ url('task')}}" method="POST" class="form-horizontal">
-                        {{ csrf_field() }}
-
-                        <!-- Task Name -->
-                        <div class="form-group">
-                            <label for="task-name" class="col-sm-3 control-label">Tâche</label>
-
-                            <div class="col-sm-6">
-                                <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
-                            </div>
-                        </div>
-
-                        <!-- Add Task Button -->
-                        <div class="form-group">
-                            <div class="col-sm-offset-3 col-sm-6">
-                                <button type="submit" class="btn btn-default">
-                                    <i class="fa fa-btn fa-plus"></i>Ajouter tâche
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-
-            <!-- Current Tasks -->
-            @if (count($tasks) > 0)
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Current Tasks
-                    </div>
-
-                    <div class="panel-body">
-                        <table class="table table-striped task-table">
-                            <thead>
-                                <th>Task</th>
-                                <th>&nbsp;</th>
-                            </thead>
-                            <tbody>
-                                @foreach ($tasks as $task)
-                                    <tr>
-                                        <td class="table-text"><div>{{ $task->name }}</div></td>
-
-                                        <!-- Task Delete Button -->
-                                        <td>
-                                            <form action="{{ url('task/'.$task->id) }}" method="POST">
-                                                {{ csrf_field() }}
-                                                {{ method_field('DELETE') }}
-
-                                                <button type="submit" class="btn btn-danger">
-                                                    <i class="fa fa-btn fa-trash"></i>Delete
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            @endif
-        </div>
-    </div>
+   <!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Parrainage</title>
+    <meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="/home/manzambi/Bureau/ASI 3.2/UML/html/fontawesome-free-5.0.11/web-fonts-with-css/css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+  </head>
+  <body>
+    <!-- Menu - Vertical -->
+    <!--  <div id = "menu-vertical">
+        <ul>
+          <li><a href="#"> Parrain/Fillot </a> </li>
+          <li><a href="#"> Profil </a> </li>
+          <li><a href="#"> Evénement </a> </li>
+          <li><a href="#">Agenda</a> </li>
+          <li><a href="#">Communication</a> </li>
+          <li><a href="#"><span id="home" class="fas fa-home"></span></a> </li>
+        </ul>
+      </div>
+    -->
+    <!-- Menu - Bannière -->
+    <section id="banniere">
+      <div class="inner">
+        <h1>Parrainage</h1>
+        <p>Site de rencontre fillot/parrain n°1</p>
+        <ul class="actions">
+          <li><a href="inscription.html" class="bouton">Inscription</a></li>
+          <li><a href="connexion.html" class="bouton">Connexion</a></li>
+        </ul>
+      </div>
+    </section>
+  </body>
+</html>
 @endsection

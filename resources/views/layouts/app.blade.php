724c4b35ebf5aefe5314c8e8a688760231bf6d95
smanzambi <36009781+smanzambi@users.noreply.github.com>
2018-05-22 20:17:16 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,1 +1,36 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Parrainage</title>
+    <meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="/home/manzambi/Bureau/ASI 3.2/UML/html/fontawesome-free-5.0.11/web-fonts-with-css/css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+  </head>
+  <body>
+    <!-- Menu - Vertical -->
+    <!--  <div id = "menu-vertical">
+        <ul>
+          <li><a href="#"> Parrain/Fillot </a> </li>
+          <li><a href="#"> Profil </a> </li>
+          <li><a href="#"> Evénement </a> </li>
+          <li><a href="#">Agenda</a> </li>
+          <li><a href="#">Communication</a> </li>
+          <li><a href="#"><span id="home" class="fas fa-home"></span></a> </li>
+        </ul>
+      </div>
+    -->
+    <!-- Menu - Bannière -->
+    <section id="banniere">
+      <div class="inner">
+        <h1>Parrainage</h1>
+        <p>Site de rencontre fillot/parrain n°1</p>
+        <ul class="actions">
+          <li><a href="inscription.html" class="bouton">Inscription</a></li>
+          <li><a href="connexion.html" class="bouton">Connexion</a></li>
+        </ul>
+      </div>
+    </section>
+  </body>
+</html>

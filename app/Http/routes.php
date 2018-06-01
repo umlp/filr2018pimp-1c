64fea70287b221a44dbf36aab9295f107d16b45d
smanzambi <sean.manzambi@insa-rouen.fr>
2018-06-02 01:32:57 +0200

    update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,30 +30,30 @@ Route::group(['middleware' => ['web']], function () {
     /**
      * Add New Task
      */
-    Route::post('/task', function (Request $request) {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|max:255',
-        ]);
-
-        if ($validator->fails()) {
-            return redirect('/')
-                ->withInput()
-                ->withErrors($validator);
-        }
-
-        $task = new Task;
-        $task->name = $request->name;
-        $task->save();
-
-        return redirect('/');
-    });
-
-    /**
-     * Delete Task
-     */
-    Route::delete('/task/{id}', function ($id) {
-        Task::findOrFail($id)->delete();
-
-        return redirect('/');
-    });
+    // Route::post('/task', function (Request $request) {
+    //     $validator = Validator::make($request->all(), [
+    //         'name' => 'required|max:255',
+    //     ]);
+    //
+    //     if ($validator->fails()) {
+    //         return redirect('/')
+    //             ->withInput()
+    //             ->withErrors($validator);
+    //     }
+    //
+    //     $task = new Task;
+    //     $task->name = $request->name;
+    //     $task->save();
+    //
+    //     return redirect('/');
+    // });
+    //
+    // /**
+    //  * Delete Task
+    //  */
+    // Route::delete('/task/{id}', function ($id) {
+    //     Task::findOrFail($id)->delete();
+    //
+    //     return redirect('/');
+    // });
 });

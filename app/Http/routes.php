951563cfb78c74bbf766b5fcbefbaba3e7307cd2
smanzambi <36009781+smanzambi@users.noreply.github.com>
2018-06-02 00:55:00 +0200

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,9 +19,7 @@ Route::group(['middleware' => ['web']], function () {
      * Show Task Dashboard
      */
     Route::get('/', function () {
-        return view('tasks', [
-            'tasks' => Task::orderBy('created_at', 'asc')->get()
-        ]);
+        return view('welcome');
     });
 
     /**

b5f41ab7b74c0216f2be60c0e7144b16241e9731
Achille Lacoin <hil@linux-2.home>
2018-06-03 22:08:36 +0200

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,24 +1,5 @@
 <?php
 
+Route::get('/', 'ListController@show');
 Route::auth();
 Route::get('/home', 'HomeController@index');
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('connexion', function () {
-    return view('connexion/connexion');
-});
-Route::get('inscription', function () {
-    return view('inscription/inscription');
-});
-Route::get('home', function () {
-    return view('home/home');
-});
-Route::get('home/agenda', function () {
-    return view('agenda/agenda');
-});
-Route::get('home/evenement', function () {
-    return view('evenement/evenement');
-});
-Route::get('/login', function () {
-});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,29 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+            <div class="panel panel-success">
+                <div class="panel-heading">List of Game of Thrones Characters</div>
 
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
+                    @if(Auth::check())
+                      <!-- Table -->
+                      <table class="table">
+                          <tr>
+                              <th>Character</th>
+                              <th>Real Name</th>
+                          </tr>
+                          @foreach($characters as $key => $value)
+                            <tr>
+                              <td>{{ $key }}</td><td>{{ $value }}</td>
+                            </tr>
+                          @endforeach
+                      </table>
+                    @endif
+
+
             </div>
+            @if(Auth::guest())
+              <a href="/login" class="btn btn-info"> You need to login to see the list 😜😜 >></a>
+            @endif
         </div>
     </div>
 </div>
